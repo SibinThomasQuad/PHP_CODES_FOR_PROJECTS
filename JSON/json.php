831285b3fb8e8e33de4c35f0c02b46315ca2d4b4
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-06-30 10:40:38 +0530

    Create json.php

---
+++ JSON/json.php
@@ -0,0 +1,27 @@
+class JSONUtils {
+    public static function addKeyValue($jsonStr, $key, $value) {
+        $jsonObj = json_decode($jsonStr, true);
+        $jsonObj[$key] = $value;
+        return json_encode($jsonObj);
+    }
+
+    public static function jsonToObject($jsonStr) {
+        return json_decode($jsonStr);
+    }
+
+    public static function jsonToArray($jsonStr) {
+        return json_decode($jsonStr, true);
+    }
+
+    public static function removeKey($jsonStr, $key) {
+        $jsonObj = json_decode($jsonStr, true);
+        if (isset($jsonObj[$key])) {
+            unset($jsonObj[$key]);
+        }
+        return json_encode($jsonObj);
+    }
+
+    public static function arrayToJson($array) {
+        return json_encode($array);
+    }
+}
