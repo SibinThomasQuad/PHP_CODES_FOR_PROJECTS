5593367e8950ee8bee18460815fb010c79df9cae
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-09-26 17:52:16 +0530

    Create md5.php

---
+++ HASHING/md5.php
@@ -0,0 +1,12 @@
+<?php
+$inputString = "Hello, World!";
+
+// Calculate MD5 hash (not recommended for security)
+$md5Hash = md5($inputString);
+
+echo "Input String: $inputString\n";
+echo "MD5 Hash: $md5Hash\n";
+
+
+
+?>
