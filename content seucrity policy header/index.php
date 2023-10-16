a28ad5334e0394f7b157f0dbaaf834a6763ab742
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-10-16 19:42:19 +0530

    Create index.php

---
+++ content seucrity policy header/index.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self';");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Page Title</title>
+    <!-- Your HTML, CSS, and JavaScript code -->
+</head>
+<body>
+    <!-- Your webpage content -->
+</body>
+</html>
